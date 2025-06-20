86562abed444b35c5088c3bd790c71f81c1959bf
Mathew Mbugua <mbuguam443@gmail.com>
2025-06-20 15:05:14 +0300

    Add temporary route to create superuser

--- core_project/urls.py
+++ core_project/urls.py
@@ -1,26 +1,8 @@
-"""
-URL configuration for core_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from django.contrib import admin
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-
-
+from django.urls import include
 
 def create_super(request):
     if not User.objects.filter(username='admin').exists():
@@ -30,5 +12,6 @@ def create_super(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('create-superuser/', create_super),  # 👈 temporary route
+    path('', include('todo.urls')),
+    path('create-superuser/', create_super),  # 👈 TEMPORARY route
 ]

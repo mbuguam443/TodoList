6b677195403bbfeba1c1dd62b2259116f97eb4db
Mathew Mbugua <mbuguam443@gmail.com>
2025-06-20 13:10:49 +0300

    Add temporary migration route

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,12 @@
 # todo/urls.py
 from django.urls import path
 from . import views
+from .views import run_migrations
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
     path('toggle/<int:task_id>/', views.toggle_task, name='toggle_task'),
     path('edit/<int:task_id>/', views.edit_task, name='edit_task'),
+    path('run-migrations/', run_migrations),
 ]
--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,16 @@ from .models import Task
 from .forms import TaskForm
 from django.shortcuts import get_object_or_404
 
+from django.core.management import call_command
+from django.http import HttpResponse
+
+
+
+def run_migrations(request):
+    call_command('migrate')
+    return HttpResponse("Migrations applied!")
+
+
 def index(request):
     tasks = Task.objects.all()
     form = TaskForm()

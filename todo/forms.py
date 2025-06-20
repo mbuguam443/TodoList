4d1d52eb82f123a3ab5a65755a3d6682602c8f93
Mathew Mbugua <mbuguam443@gmail.com>
2025-06-20 10:27:41 +0300

    Initial commit - Django ToDo List

---
+++ todo/forms.py
@@ -0,0 +1,8 @@
+# todo/forms.py
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'completed']
---
+++ todo/views.py
@@ -0,0 +1,14 @@
+# todo/views.py
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    return render(request, 'todo/index.html', {'tasks': tasks, 'form': form})

860d9b701aab98ac174d503afab834d420412964
Mathew Mbugua <mbuguam443@gmail.com>
2025-06-20 10:48:00 +0300

    Add task deletion feature

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
+from django.shortcuts import get_object_or_404
 
 def index(request):
     tasks = Task.objects.all()
@@ -12,3 +13,9 @@ def index(request):
             form.save()
             return redirect('/')
     return render(request, 'todo/index.html', {'tasks': tasks, 'form': form})
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('index')

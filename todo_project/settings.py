6d8e24be4e3ab8b2ee3f92153725e5bfd13888e7
Mathew Mbugua <mbuguam443@gmail.com>
2025-06-20 12:47:10 +0300

    Switched to PostgreSQL

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tododb',
+        'USER': 'todo_user',
+        'PASSWORD': 'todo_pass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

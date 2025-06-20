f73c07c8199f42ca714709482d9cbec3a7622a4c
Mathew Mbugua <mbuguam443@gmail.com>
2025-06-20 11:11:33 +0300

    Add edit task feature

--- todo/urls.py
+++ todo/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
     path('toggle/<int:task_id>/', views.toggle_task, name='toggle_task'),
+    path('edit/<int:task_id>/', views.edit_task, name='edit_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -25,3 +25,15 @@ def toggle_task(request, task_id):
     task.completed = not task.completed
     task.save()
     return redirect('index')
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = TaskForm(instance=task)
+    
+    return render(request, 'todo/edit.html', {'form': form, 'task': task})
